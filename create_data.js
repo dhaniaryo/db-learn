f57a0521f739811e5a4b11f5d976bf7a590751f1
dhaniaryo <dhanipriyono01@gmail.com>
2021-08-23 01:16:58 +0700

    adding some data using insertMany method

--- create_data.js
+++ create_data.js
@@ -28,10 +28,47 @@ const apple = new Fruit(
     }
 );
 
-apple.save(function(error, apple){
+// apple.save(function(error, apple){
+//     if(error){
+//         console.log(error);
+//     } else {
+//         console.log(`Adding ${apple.name} is success!`);
+//     }
+// });
+
+
+const pear = new Fruit(
+    {
+        name: "Pear",
+        origin: "Malang",
+        rating: 95,
+        review: "This fruit tastes sweet and delicious!"
+    }
+);
+
+const melon = new Fruit(
+    {
+        name: "Melon",
+        origin: "Malang",
+        rating: 95,
+        review: "This fruit tastes sweet and delicious!"
+    }
+);
+
+const banana = new Fruit(
+    {
+        name: "Banana",
+        origin: "Malang",
+        rating: 90,
+        review: "This fruit tastes sweet!"
+    }
+);
+
+Fruit.insertMany([apple,pear, melon, banana], function(error){
     if(error){
         console.log(error);
     } else {
-        console.log(`Adding ${apple.name} is success!`);
+        mongoose.connection.close();
+        console.log(`Adding ${apple.name}, ${pear.name}, ${melon.name}, ${banana.name} is success!`);
     }
 });
